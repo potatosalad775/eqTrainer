eaa02db4d45279fe68c1ab6b5ee3464c7c8d4731
Jinwoo Lee <blackbox0209@gmail.com>
2024-10-02 01:36:48 +0900

    Chore: Edited playlist string for Windows
    Audio files except .wav extension is not working on Windows ATM.

--- lib/page/playlist_page.dart
+++ lib/page/playlist_page.dart
@@ -19,136 +19,148 @@ class _PlaylistPageState extends State<PlaylistPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ValueListenableBuilder(
-        valueListenable: Hive.box<AudioClip>(audioClipBoxName).listenable(),
-        builder: (context, Box<AudioClip> box, _) {
-          if(box.values.isEmpty) {
-            return Center(
-              child: Text("PLAYLIST_EMPTY_TEXT".tr()),
-            );
-          }
-          else {
-            return ReorderableListView.builder(
-              buildDefaultDragHandles: false,
-              padding: const EdgeInsets.all(5),
-              itemCount: box.values.length,
-              itemBuilder: (context, index) {
-                AudioClip? currentClip = box.getAt(index);
-                Duration clipDuration = Duration(milliseconds: (currentClip!.duration * 1000).toInt());
-                return Padding(
-                  key: Key("$index"),
-                  padding: const EdgeInsets.all(3),
-                  child: Card(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                    color: Theme.of(context).colorScheme.surfaceContainer,
-                    child: ListTile(
-                      minLeadingWidth: 0,
-                      title: Text(
-                        currentClip.ogAudioName,
-                        maxLines: 1,
-                        softWrap: false,
-                        overflow: TextOverflow.fade,
-                      ),
-                      subtitle: Text(clipDuration.toString().substring(2, 10)),
-                      leading: ReorderableDragStartListener(
-                        index: index,
-                        child: IconButton(
-                          onPressed: () {
-                            currentClip.isEnabled = !currentClip.isEnabled;
-                            box.putAt(index, currentClip);
-                          },
-                          icon: Icon(currentClip.isEnabled ? Icons.radio_button_checked : Icons.radio_button_unchecked),
+          valueListenable: Hive.box<AudioClip>(audioClipBoxName).listenable(),
+          builder: (context, Box<AudioClip> box, _) {
+            if (box.values.isEmpty) {
+              return Center(
+                child: Text("PLAYLIST_EMPTY_TEXT".tr()),
+              );
+            } else {
+              return ReorderableListView.builder(
+                buildDefaultDragHandles: false,
+                padding: const EdgeInsets.all(5),
+                itemCount: box.values.length,
+                itemBuilder: (context, index) {
+                  AudioClip? currentClip = box.getAt(index);
+                  Duration clipDuration = Duration(
+                      milliseconds: (currentClip!.duration * 1000).toInt());
+                  return Padding(
+                    key: Key("$index"),
+                    padding: const EdgeInsets.all(3),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      color: Theme.of(context).colorScheme.surfaceContainer,
+                      child: ListTile(
+                        minLeadingWidth: 0,
+                        title: Text(
+                          currentClip.ogAudioName,
+                          maxLines: 1,
+                          softWrap: false,
+                          overflow: TextOverflow.fade,
+                        ),
+                        subtitle:
+                            Text(clipDuration.toString().substring(2, 10)),
+                        leading: ReorderableDragStartListener(
+                          index: index,
+                          child: IconButton(
+                            onPressed: () {
+                              currentClip.isEnabled = !currentClip.isEnabled;
+                              box.putAt(index, currentClip);
+                            },
+                            icon: Icon(currentClip.isEnabled
+                                ? Icons.radio_button_checked
+                                : Icons.radio_button_unchecked),
+                          ),
+                        ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            // Play Button
+                            IconButton(
+                              onPressed: () {
+                                // Show popup screen
+                                showModalBottomSheet(
+                                    isDismissible: false,
+                                    enableDrag: false,
+                                    useSafeArea: true,
+                                    context: context,
+                                    isScrollControlled: true,
+                                    builder: (context) {
+                                      if (Platform.isWindows) {
+                                        return PlaylistControlView(
+                                            filePath:
+                                                "${audioClipDir.path}\\${box.getAt(index)?.fileName}");
+                                      } else {
+                                        return PlaylistControlView(
+                                            filePath:
+                                                "${audioClipDir.path}/${box.getAt(index)?.fileName}");
+                                      }
+                                    });
+                              },
+                              icon: const Icon(Icons.play_arrow),
+                            ),
+                            // More Menu Button
+                            IconButton(
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (_) => SimpleDialog(
+                                    title:
+                                        Text("PLAYLIST_AUDIO_ALERT_TITLE".tr()),
+                                    children: [
+                                      TextButton(
+                                        onPressed: () {
+                                          box.deleteAt(index);
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text(
+                                            "PLAYLIST_AUDIO_ALERT_ACTION_DELETE"
+                                                .tr()),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                              icon: const Icon(Icons.more_vert),
+                            ),
+                          ],
                         ),
                       ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          // Play Button
-                          IconButton(
-                            onPressed: () {
-                              // Show popup screen
-                              showModalBottomSheet(
-                                isDismissible: false,
-                                enableDrag: false,
-                                useSafeArea: true,
-                                context: context,
-                                isScrollControlled: true,
-                                builder: (context) {
-                                  if(Platform.isWindows) {
-                                    return PlaylistControlView(
-                                        filePath: "${audioClipDir.path}\\${box.getAt(index)?.fileName}"
-                                    );
-                                  } else {
-                                    return PlaylistControlView(
-                                      filePath: "${audioClipDir.path}/${box.getAt(index)?.fileName}"
-                                    );
-                                  }
-                                }
-                              );
-                            },
-                            icon: const Icon(Icons.play_arrow),
-                          ),
-                          // More Menu Button
-                          IconButton(
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (_) => SimpleDialog(
-                                  title: Text("PLAYLIST_AUDIO_ALERT_TITLE".tr()),
-                                  children: [
-                                    TextButton(
-                                      onPressed: () {
-                                        box.deleteAt(index);
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text("PLAYLIST_AUDIO_ALERT_ACTION_DELETE".tr()),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                            icon: const Icon(Icons.more_vert),
-                          ),
-                        ],
-                      ),
                     ),
-                  ),
-                );
-              },
-              onReorder: (int oldIndex, int newIndex) {
-                if(oldIndex < newIndex) {
-                  newIndex -= 1;
-                }
-                setState(() {
-                  final oldItem = box.getAt(oldIndex);
-                  final newItem = box.getAt(newIndex);
+                  );
+                },
+                onReorder: (int oldIndex, int newIndex) {
+                  if (oldIndex < newIndex) {
+                    newIndex -= 1;
+                  }
+                  setState(() {
+                    final oldItem = box.getAt(oldIndex);
+                    final newItem = box.getAt(newIndex);
 
-                  box.putAt(oldIndex, newItem!);
-                  box.putAt(newIndex, oldItem!);
-                });
-              },
-              footer: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text("PLAYLIST_FOOTER").tr(),
-                    if(Platform.isWindows || Platform.isLinux) const SizedBox(height: 12),
-                    if(Platform.isWindows || Platform.isLinux) const Text("PLAYLIST_FOOTER_WINLINUX_1").tr(),
-                    if(Platform.isWindows || Platform.isLinux) const Text("PLAYLIST_FOOTER_WINLINUX_2").tr()
-                  ],
+                    box.putAt(oldIndex, newItem!);
+                    box.putAt(newIndex, oldItem!);
+                  });
+                },
+                footer: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text("PLAYLIST_FOOTER").tr(),
+                      if (Platform.isWindows || Platform.isLinux)
+                        const SizedBox(height: 12),
+                      if (Platform.isWindows)
+                        const Text("PLAYLIST_FOOTER_WIN_1").tr(),
+                      if (Platform.isWindows)
+                        const Text("PLAYLIST_FOOTER_WIN_2").tr(),
+                      if (Platform.isLinux)
+                        const Text("PLAYLIST_FOOTER_LINUX_1").tr(),
+                      if (Platform.isLinux)
+                        const Text("PLAYLIST_FOOTER_LINUX_2").tr(),
+                    ],
+                  ),
                 ),
-              ),
-            );
-          }
-        }
-      ),
+              );
+            }
+          }),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ImportPage()));
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const ImportPage()));
         },
       ),
     );
   }
 }
-
